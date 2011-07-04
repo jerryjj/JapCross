8c109c29d5867d69213b033b585d1f28958f0792
Jerry Jalava <jerry.jalava@iki.fi>
2011-07-05 00:22:29 +0300

    Added English and Finnish localizations

---
+++ qml-translations.en.ts
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>GameBoard</name>
+    <message>
+        <location filename="qml/JapCross/GameBoard.qml" line="56"/>
+        <source>Continue</source>
+        <translation>Continue</translation>
+    </message>
+</context>
+<context>
+    <name>LevelList</name>
+    <message>
+        <location filename="qml/JapCross/Menu/LevelList.qml" line="73"/>
+        <source>Time spent</source>
+        <translation>Time spent</translation>
+    </message>
+    <message>
+        <location filename="qml/JapCross/Menu/LevelList.qml" line="93"/>
+        <source>Author</source>
+        <translation>Author</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <location filename="qml/JapCross/Menu/Main.qml" line="44"/>
+        <source>Instructions</source>
+        <translation>Instructions</translation>
+    </message>
+    <message>
+        <location filename="qml/JapCross/Menu/Main.qml" line="53"/>
+        <source>Play</source>
+        <translation>Play</translation>
+    </message>
+    <message>
+        <location filename="qml/JapCross/Menu/Main.qml" line="85"/>
+        <location filename="qml/JapCross/Menu/Main.qml" line="137"/>
+        <source>Back to menu</source>
+        <translation>Back to menu</translation>
+    </message>
+    <message>
+        <location filename="qml/JapCross/Menu/Main.qml" line="129"/>
+        <source>game_instructions</source>
+        <translation>Nonograms, also known as Hanjie, Paint by Numbers, or Griddlers, are picture logic puzzles in which cells in a grid have to be colored or left blank according to numbers given at the side of the grid to reveal a hidden picture.&lt;br&gt;&lt;br&gt;In this puzzle type, the numbers measure how many unbroken lines of filled-in squares there are in any given row or column.&lt;br&gt;&lt;br&gt;For example, a clue of &quot;4 8 3&quot; would mean there are sets of four, eight, and three filled squares, in that order, with at least one blank square between successive groups. &lt;br&gt;&lt;br&gt;&lt;strong&gt;- Wikipedia&lt;/strong&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="qml/JapCross/main.qml" line="68"/>
+        <source>Loading level</source>
+        <translation>Loading Level</translation>
+    </message>
+    <message>
+        <location filename="qml/JapCross/main.qml" line="133"/>
+        <source>Really clear the level?</source>
+        <translation>Really clear the level?</translation>
+    </message>
+    <message>
+        <location filename="qml/JapCross/main.qml" line="147"/>
+        <source>Yes</source>
+        <translation>Yes</translation>
+    </message>
+    <message>
+        <location filename="qml/JapCross/main.qml" line="157"/>
+        <source>Cancel</source>
+        <translation>Cancel</translation>
+    </message>
+</context>
+</TS>
---
+++ qml-translations.fi.ts
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fi_FI">
+<context>
+    <name>GameBoard</name>
+    <message>
+        <location filename="qml/JapCross/GameBoard.qml" line="56"/>
+        <source>Continue</source>
+        <translation>Jatka</translation>
+    </message>
+</context>
+<context>
+    <name>LevelList</name>
+    <message>
+        <location filename="qml/JapCross/Menu/LevelList.qml" line="73"/>
+        <source>Time spent</source>
+        <translation>Aikaa käytetty</translation>
+    </message>
+    <message>
+        <location filename="qml/JapCross/Menu/LevelList.qml" line="93"/>
+        <source>Author</source>
+        <translation>Julkaisija</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <location filename="qml/JapCross/Menu/Main.qml" line="44"/>
+        <source>Instructions</source>
+        <translation>Ohjeet</translation>
+    </message>
+    <message>
+        <location filename="qml/JapCross/Menu/Main.qml" line="53"/>
+        <source>Play</source>
+        <translation>Pelaa</translation>
+    </message>
+    <message>
+        <location filename="qml/JapCross/Menu/Main.qml" line="85"/>
+        <location filename="qml/JapCross/Menu/Main.qml" line="137"/>
+        <source>Back to menu</source>
+        <translation>Takaisin valikkoon</translation>
+    </message>
+    <message>
+        <location filename="qml/JapCross/Menu/Main.qml" line="129"/>
+        <source>game_instructions</source>
+        <translation>Japanilainen ristikko, tunnetaan myös nimellä Nonogram, Hanjie on ristikkotyyppi, jossa on tarkoituksena värittää ristikon ruuduista vihjeiden avulla ruutuja. Väritetyistä ruuduista muodostuu kuva.&lt;br&gt;&lt;br&gt;Ruuturivien ylä- ja sivumarginaaleissa kerrotaan vihjeenä kunkin rivin peräkkäisten väritettävien ruutujen määrä.&lt;br&gt;Väritettyjen ruuturyhmien väliin on aina jätettävä vähintään yksi tyhjä ruutu.&lt;br&gt;&lt;br&gt;Esimerkiksi vihje ”3 6 2” tarkoittaa, että rivillä on yksi kolmen, yksi kuuden ja yksi kahden värillisen ruudun ruuturyhmä.&lt;br&gt;&lt;br&gt;&lt;strong&gt;- Wikipedia&lt;/strong&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="qml/JapCross/main.qml" line="68"/>
+        <source>Loading level</source>
+        <translation>Ladataan tasoa</translation>
+    </message>
+    <message>
+        <location filename="qml/JapCross/main.qml" line="133"/>
+        <source>Really clear the level?</source>
+        <translation>Tyhjennetäänkö taso?</translation>
+    </message>
+    <message>
+        <location filename="qml/JapCross/main.qml" line="147"/>
+        <source>Yes</source>
+        <translation>Kyllä</translation>
+    </message>
+    <message>
+        <location filename="qml/JapCross/main.qml" line="157"/>
+        <source>Cancel</source>
+        <translation>Peruuta</translation>
+    </message>
+</context>
+</TS>
